7b47a63f4b953134febfe0dbfd29c987ec478d62
maxima <maxima.at.work@gmail.com>
2026-05-08 18:25:44 +0300

    Implement MyThread class & main method with ExecutorService.

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -2,15 +2,34 @@ package core.basesyntax;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
+import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class Main {
     private static final Logger logger = LogManager.getLogger(Main.class);
 
-    public static void main(String[] args) {
-        List<Future<String>> futures = new ArrayList<>();
-        // write your code here
+    public static void main(String[] args) throws InterruptedException {
+        ExecutorService executorService = Executors.newFixedThreadPool(5);
+        List<Callable<String>> tasks = new ArrayList<>();
+        for (int i = 0; i < 20; i++) {
+            tasks.add(new MyThread());
+        }
+
+        List<Future<String>> futures = executorService.invokeAll(tasks);
+        futures.forEach(f -> {
+            try {
+                logger.log(Level.INFO, f.get());
+            } catch (InterruptedException | ExecutionException e) {
+                Thread.currentThread().interrupt();
+            }
+        });
+
+        executorService.shutdown();
     }
 }
--- src/main/java/core/basesyntax/MyThread.java
+++ src/main/java/core/basesyntax/MyThread.java
@@ -1,11 +1,15 @@
 package core.basesyntax;
 
+import java.time.LocalTime;
+import java.util.Random;
 import java.util.concurrent.Callable;
 
 public class MyThread implements Callable<String> {
     @Override
-    public String call() throws InterruptedException {
-        // write your code here
-        return null;
+    public String call() {
+        Random randomizer = new Random();
+        int worktime = randomizer.nextInt(1000);
+        return String.format("Task duration was %d ms, execution finished at %tT:%<tL",
+                worktime, LocalTime.now());
     }
 }
